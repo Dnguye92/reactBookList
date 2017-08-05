f062969c0bf6d40b53fbb128dbc6b4c2ebddf2c1
Dylan Nguyen <Dnguye92@yahoo.com>
2017-08-04 21:47:50 -0700

    wrapping up small Book Selecting App

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{ title: 'JavaScript: The Good Parts' },
-		{ title: 'Harry Potter' },
-		{ title: 'The Dark Tower' },
-		{ title: 'Eloquen JavaScript' }
+		{ title: 'JavaScript: The Good Parts', pages: 101 },
+		{ title: 'Harry Potter', pages: 800 },
+		{ title: 'The Dark Tower', pages: 684 },
+		{ title: 'Eloquent JavaScript', pages: 312 }
 	]
 }
